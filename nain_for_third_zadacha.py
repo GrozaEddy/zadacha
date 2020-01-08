a964b4e109f8af041ae6025b1aa5a6aa32d98d76
Эдуард <evoronin2002@yandex.ru>
2020-01-08 21:38:09 +0300

    main file for third zadacha

---
+++ nain_for_third_zadacha.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from random import choice
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.x = self.y = self.r = 0
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.r = choice(list(range(50, 300)))
+        self.x, self.y = choice(list(range(50, 550))), choice(list(range(50, 550)))
+        self.paintEvent(self.event)
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw(qp)
+        qp.end()
+
+    def draw(self, qp):
+        r, g, b = choice(list(range(0, 255))), choice(list(range(0, 255))), choice(
+            list(range(0, 255)))
+        qp.setBrush(QColor(r, g, b))
+        qp.drawEllipse(self.x, self.y, self.r, self.r)
+        self.update()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
