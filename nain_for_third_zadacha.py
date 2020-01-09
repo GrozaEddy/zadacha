8b4828445e9023c7d16a1117e3fa3b1ee049c657
Эдуард <evoronin2002@yandex.ru>
2020-01-09 21:10:32 +0300

    third zadacha

--- nain_for_third_zadacha.py
+++ nain_for_third_zadacha.py
@@ -3,18 +3,7 @@ from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from random import choice
 from PyQt5.QtWidgets import QApplication, QMainWindow
-
-
-class MyWidget(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('Ui.ui', self)
-        self.x = self.y = self.r = 0import sys
-from PyQt5 import uic
-from PyQt5.QtGui import QPainter, QColor
-from random import choice
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from ui_file import Ui_MainWindow
+from ui_file2 import Ui_MainWindow
 
 
 class MyWidget(QMainWindow, Ui_MainWindow):
@@ -22,33 +11,6 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.x = self.y = self.r = 0
-        self.pushButton.clicked.connect(self.run)
-
-    def run(self):
-        self.r = choice(list(range(50, 300)))
-        self.x, self.y = choice(list(range(50, 550))), choice(list(range(50, 550)))
-        self.paintEvent(self.event)
-
-    def paintEvent(self, event):
-        qp = QPainter()
-        qp.begin(self)
-        self.draw(qp)
-        qp.end()
-
-    def draw(self, qp):
-        r, g, b = choice(list(range(0, 256))), choice(list(range(0, 256))), choice(
-            list(range(0, 256)))
-        qp.setBrush(r, g, b)
-        qp.drawEllipse(self.x, self.y, self.r, self.r)
-        self.update()
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
-    sys.exit(app.exec_())
-
         self.pushButton.clicked.connect(self.run)
 
     def run(self):
